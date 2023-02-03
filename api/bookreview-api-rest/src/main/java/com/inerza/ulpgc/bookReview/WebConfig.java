63470f2d58f7e552bea48fbd375cd91a71f7ed9f
Jesús Rodríguez <jrodriguez@inerza.com>
2023-02-03 12:21:41 +0000

    Añadir localhost (puerto 80) a configuración CORS

--- api/bookreview-api-rest/src/main/java/com/inerza/ulpgc/bookReview/WebConfig.java
+++ api/bookreview-api-rest/src/main/java/com/inerza/ulpgc/bookReview/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer  {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8100")
+                .allowedOrigins("http://localhost:8100","http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowCredentials(true);
     }
